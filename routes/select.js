b0c8b64ec20e9b0208c05338f03ffd58793441e8
Na-Nazhou <nazhou.na9@gmail.com>
2019-10-28 18:45:02 +0800

    Add dotenv

--- routes/select.js
+++ routes/select.js
@@ -2,12 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 const { Pool } = require('pg')
+/* --- V7: Using Dot Env ---
 const pool = new Pool({
   host: 'localhost',
   database: 'postgres',
   password: '',
   port: 5432,
 })
+*/
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL
+});
+
 
 /* SQL Query */
 var sql_query = 'SELECT * FROM student_info';
