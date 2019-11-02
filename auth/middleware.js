9756de7f66bf9eefd60d50053c57b57b7bfe6321
Na-Nazhou <e0148669@u.nus.edu>
2019-11-03 05:45:22 +0800

    Refactor canEditUser

--- auth/middleware.js
+++ auth/middleware.js
@@ -1,3 +1,9 @@
+const handleAccessDenied = (req, res, msg) => {
+  req.flash('error', `Access Denied: ${msg}`);
+  res.status(403);
+  res.redirect('back');
+};
+
 exports.ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
@@ -18,8 +24,14 @@ exports.ensureIsAdmin = (req, res, next) => {
   if (req.user.is_admin) {
     next();
   } else {
-    res.status(403);
-    req.flash('error', 'Access Denied');
-    res.redirect('back');
+    handleAccessDenied(req, res, 'Only admin is authorised to access');
+  }
+};
+
+exports.authorisedToEditUser = (req, res, next) => {
+  if (req.user.is_admin || req.params.id === req.user.id) {
+    next();
+  } else {
+    handleAccessDenied(req, res, 'Unauthorised to edit this user');
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,10 @@
 const router = require('express').Router();
 const users = require('../controllers/users_controller');
-const { ensureIsAdmin } = require('../auth/middleware');
-
-/* PERMISSIONS */
-const canEditUser = (req, res, next) => {
-    if(req.user.is_admin || req.params.id === req.user.id) {
-        next();
-    } else {
-        res.status(403);
-        req.flash('error', 'Unauthorized to edit this user');
-        res.redirect('back');
-    }
-}
+const { ensureIsAdmin, authorisedToEditUser } = require('../auth/middleware');
 
 router.get('/', users.index);
 router.delete('/:id', ensureIsAdmin, users.delete);
-router.get('/:id/edit', canEditUser, users.edit);
-router.put('/:id', canEditUser, users.update);
+router.get('/:id/edit', authorisedToEditUser, users.edit);
+router.put('/:id', authorisedToEditUser, users.update);
 
 module.exports = router;
