1761cf98b60e5e79c6a85d6df4d83538b8e83a44
Na-Nazhou <e0148669@u.nus.edu>
2019-11-03 14:35:09 +0800

    Add course permissions

--- auth/middleware.js
+++ auth/middleware.js
@@ -1,3 +1,6 @@
+const db = require('../db');
+const course_memberships = require('../sql/course_memberships');
+
 const handleAccessDenied = (req, res, msg) => {
   req.flash('error', `Access Denied: ${msg}`);
   res.status(403);
@@ -33,5 +36,26 @@ exports.authorisedToEditUser = (req, res, next) => {
     next();
   } else {
     handleAccessDenied(req, res, 'Unauthorised to edit this user');
+  }
+};
+
+exports.authorisedToEditCourse = (req, res, next) => {
+  if (req.user.is_admin) {
+    next();
+  } else {
+    db.query(
+      course_memberships.functions.is_member_in_course,
+      [req.user.id, req.params.semester_name, req.params.module_code, 'professor'],
+      (err, data) => {
+        if (err) {
+          req.flash('error', err.message);
+          res.redirect('back');
+        } else if (data.rows[0].is_member_in_course) {
+          next();
+        } else {
+          handleAccessDenied(req, res, 'Unauthorised to edit this course');
+        }
+      }
+    );
   }
 };
