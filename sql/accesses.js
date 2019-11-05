39e1ddf265fe63ad0f59f0ede8fb61ae4119f5f1
Na-Nazhou <nazhou.na9@gmail.com>
2019-11-05 23:56:13 +0800

    Add routes and basic view

--- sql/accesses.js
+++ sql/accesses.js
@@ -3,7 +3,11 @@ const accesses = {};
 accesses.queries = {
   find_accesss:
     'SELECT * FROM accesses A NATURAL JOIN group_memberships GM' +
-    'WHERE semester_name=$1 AND module_code=$2 AND forum_title=$3 AND user_id=4'
+    'WHERE semester_name=$1 AND module_code=$2 AND forum_title=$3 AND user_id=4',
+  get_group_names_by_forum:
+    'SELECT group_name FROM accesses A WHERE A.semester_name = $1 AND A.module_code = $2 AND A.forum_title = $3'
 };
+
+accesses.functions = {};
 
 module.exports = accesses;
