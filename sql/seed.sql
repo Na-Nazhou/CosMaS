60a32466261e32afc832b71dbd09536874d44063
Na-Nazhou <nazhou.na9@gmail.com>
2019-11-06 16:50:58 +0800

    Add seed data

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,12 +1,12 @@
 /* USERS */
 INSERT INTO users VALUES ('A9999999A', 'Admin', 'True', '$2b$10$T24mdVF8M8ie28Rkj8hHkuoftnRbVNLZMvPMkJ7TaOpbfsQC2EvMW');
-INSERT INTO users (id, name, password_digest) VALUES ('A0130001B', 'Chloe Tan', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
-INSERT INTO users (id, name, password_digest) VALUES ('A0140002C', 'Timothy Phua', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
-INSERT INTO users (id, name, password_digest) VALUES ('A0150003D', 'Dana Zhang', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
-INSERT INTO users (id, name, password_digest) VALUES ('A0160004E', 'Megan Tan', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
-INSERT INTO users (id, name, password_digest) VALUES ('A0160005F', 'Reuben Lam', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
-INSERT INTO users (id, name, password_digest) VALUES ('A0170006G', 'Michelle Yeoh', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
-INSERT INTO users (id, name, password_digest) VALUES ('A0180007H', 'Albert Ang', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
+INSERT INTO users (id, name, password_digest) VALUES ('A0160001B', 'Chloe Tan', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
+INSERT INTO users (id, name, password_digest) VALUES ('A0160002C', 'Timothy Phua', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
+INSERT INTO users (id, name, password_digest) VALUES ('A0170003D', 'Dana Zhang', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
+INSERT INTO users (id, name, password_digest) VALUES ('A0170004E', 'Megan Tan', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
+INSERT INTO users (id, name, password_digest) VALUES ('A0180005F', 'Reuben Lam', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
+INSERT INTO users (id, name, password_digest) VALUES ('A0180015F', 'Michelle Yeoh', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
+INSERT INTO users (id, name, password_digest) VALUES ('A0190007H', 'Albert Ang', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
 INSERT INTO users (id, name, password_digest) VALUES ('A0190008I', 'Rebecca Chua', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
 INSERT INTO users (id, name, password_digest) VALUES ('A0200009J', 'Ashley Chee', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
 INSERT INTO users (id, name, password_digest) VALUES ('A0200000K', 'Patrick Leow', '$2b$10$xpXE/.ugaLhigMWJBjmXFOLId8l7YpVSC0DaV69MS//BuDhXbEwlq');
@@ -29,16 +29,93 @@ INSERT INTO courses VALUES ('AY18/19S2', 'CS2102', 'Database Systems', 'The aim 
 INSERT INTO courses VALUES ('AY18/19S1', 'CS1101S', 'Programming Methodology', 'This module introduces the concepts of programming and computational problem solving, and is the first and foremost introductory module to computing. Starting from a small core of fundamental abstractions, the module introduces programming as a method for communicating computational processes. The module begins with purely functional programming based on a simple substitution-based execution model, and ends with a powerful modern imperative language based on a realistic environment-based execution model. Topics covered include: functional abstraction, recursion, higher-order functions, data abstraction, algorithmic strategies, state mutation, loops and arrays, evaluation strategies, sorting and searching, debugging and testing.', 5);
 INSERT INTO courses VALUES ('AY18/19S2', 'CS1101S', 'Programming Methodology (Revamped)', 'This module introduces the concepts of programming and computational problem solving, and is the first and foremost introductory module to computing. Starting from a small core of fundamental abstractions, the module introduces programming as a method for communicating computational processes. The module begins with purely functional programming based on a simple substitution-based execution model, and ends with a powerful modern imperative language based on a realistic environment-based execution model. Topics covered include: functional abstraction, recursion, higher-order functions, data abstraction, algorithmic strategies, state mutation, loops and arrays, evaluation strategies, sorting and searching, debugging and testing.', 4);
 INSERT INTO courses VALUES ('AY18/19S1', 'CS2103', 'Software Engineering', 'This module introduces the necessary conceptual and analytical tools for systematic and rigorous development of software systems. It covers four main areas of software development, namely object-oriented system analysis, object-oriented system modelling and design, implementation, and testing, with emphasis on system modelling and design and implementation of software modules that work cooperatively to fulfill the requirements of the system. Tools and techniques for software development, such as Unified Modelling Language (UML), program specification, and testing methods, will be taught. Major software engineering issues such as modularisation criteria, program correctness, and software quality will also be covered.', 4);
+INSERT INTO courses VALUES ('AY19/20S1', 'CS2103', 'Software Engineering', 'This module introduces the necessary conceptual and analytical tools for systematic and rigorous development of software systems. It covers four main areas of software development, namely object-oriented system analysis, object-oriented system modelling and design, implementation, and testing, with emphasis on system modelling and design and implementation of software modules that work cooperatively to fulfill the requirements of the system. Tools and techniques for software development, such as Unified Modelling Language (UML), program specification, and testing methods, will be taught. Major software engineering issues such as modularisation criteria, program correctness, and software quality will also be covered.', 6);
+INSERT INTO courses VALUES ('AY18/19S1', 'GEQ1000', 'Asking Questions', 'There are many ways to ask questions, and many kinds of questions that different disciplines investigate. For a start, this module introduces six dominant modes of questioning from the perspective of computational thinking, design thinking, engineering, philosophy, science, and social sciences. These six perspectives serve as a starting point to introduce all undergraduate students to different modes of questioning across these disciplines, and provide an initial exposure to how scholars from these disciplines pursue specific lines of questioning of everyday issues. We emphasize that while there is only limited time and space within one module to devote to specific disciplinary lines of investigations, we encourage all students to actively think about other lines of questioning, other questions that need to be asked, particularly in disciplines not represented in this introductory platform as we move through this journey together. We expect that in future subsequent offerings, other disciplinary modes of investigations may also be introduced.', 4);
+INSERT INTO courses VALUES ('AY18/19S2', 'GEQ1000', 'Asking Questions', 'There are many ways to ask questions, and many kinds of questions that different disciplines investigate. For a start, this module introduces six dominant modes of questioning from the perspective of computational thinking, design thinking, engineering, philosophy, science, and social sciences. These six perspectives serve as a starting point to introduce all undergraduate students to different modes of questioning across these disciplines, and provide an initial exposure to how scholars from these disciplines pursue specific lines of questioning of everyday issues. We emphasize that while there is only limited time and space within one module to devote to specific disciplinary lines of investigations, we encourage all students to actively think about other lines of questioning, other questions that need to be asked, particularly in disciplines not represented in this introductory platform as we move through this journey together. We expect that in future subsequent offerings, other disciplinary modes of investigations may also be introduced.', 4);
+INSERT INTO courses VALUES ('AY19/20S1', 'GEQ1000', 'Asking Questions', 'There are many ways to ask questions, and many kinds of questions that different disciplines investigate. For a start, this module introduces six dominant modes of questioning from the perspective of computational thinking, design thinking, engineering, philosophy, science, and social sciences. These six perspectives serve as a starting point to introduce all undergraduate students to different modes of questioning across these disciplines, and provide an initial exposure to how scholars from these disciplines pursue specific lines of questioning of everyday issues. We emphasize that while there is only limited time and space within one module to devote to specific disciplinary lines of investigations, we encourage all students to actively think about other lines of questioning, other questions that need to be asked, particularly in disciplines not represented in this introductory platform as we move through this journey together. We expect that in future subsequent offerings, other disciplinary modes of investigations may also be introduced.', 4);
+INSERT INTO courses VALUES ('AY19/20S2', 'GEQ1000', 'Asking Questions', 'There are many ways to ask questions, and many kinds of questions that different disciplines investigate. For a start, this module introduces six dominant modes of questioning from the perspective of computational thinking, design thinking, engineering, philosophy, science, and social sciences. These six perspectives serve as a starting point to introduce all undergraduate students to different modes of questioning across these disciplines, and provide an initial exposure to how scholars from these disciplines pursue specific lines of questioning of everyday issues. We emphasize that while there is only limited time and space within one module to devote to specific disciplinary lines of investigations, we encourage all students to actively think about other lines of questioning, other questions that need to be asked, particularly in disciplines not represented in this introductory platform as we move through this journey together. We expect that in future subsequent offerings, other disciplinary modes of investigations may also be introduced.', 4);
 
 /* COURSE_REQUESTS */
 
 /* COURSE_MEMBERSHIPS */
+INSERT INTO course_memberships VALUES ('student', 'AY18/19S1', 'CS2102', 'A0160001B');
+INSERT INTO course_memberships VALUES ('student', 'AY18/19S1', 'CS2102', 'A0160002C');
+INSERT INTO course_memberships VALUES ('student', 'AY18/19S1', 'CS2102', 'A0170003D');
+INSERT INTO course_memberships VALUES ('student', 'AY18/19S1', 'CS2102', 'A0170004E');
+INSERT INTO course_memberships VALUES ('TA', 'AY18/19S1', 'CS2102', 'A0180005F');
+INSERT INTO course_memberships VALUES ('TA', 'AY18/19S1', 'CS2102', 'A0180015F');
 
 /* GROUPS */
+INSERT INTO groups VALUES ('Group 1', 'AY18/19S1', 'CS2102');
+INSERT INTO groups VALUES ('Group 2', 'AY18/19S1', 'CS2102');
+INSERT INTO groups VALUES ('Group 3', 'AY18/19S1', 'CS2102');
+INSERT INTO groups VALUES ('Group 1', 'AY18/19S2', 'CS2102');
+INSERT INTO groups VALUES ('Group 2', 'AY18/19S2', 'CS2102');
+INSERT INTO groups VALUES ('Group 3', 'AY18/19S2', 'CS2102');
+
+INSERT INTO groups VALUES ('Group 1', 'AY18/19S1', 'CS1101S');
+INSERT INTO groups VALUES ('Group 2', 'AY18/19S1', 'CS1101S');
+INSERT INTO groups VALUES ('Group 3', 'AY18/19S1', 'CS1101S');
+INSERT INTO groups VALUES ('Group 1', 'AY18/19S2', 'CS1101S');
+INSERT INTO groups VALUES ('Group 2', 'AY18/19S2', 'CS1101S');
+INSERT INTO groups VALUES ('Group 3', 'AY18/19S2', 'CS1101S');
+
+INSERT INTO groups VALUES ('Group 1', 'AY18/19S1', 'CS2103');
+INSERT INTO groups VALUES ('Group 2', 'AY18/19S1', 'CS2103');
+INSERT INTO groups VALUES ('Group 3', 'AY18/19S1', 'CS2103');
+INSERT INTO groups VALUES ('Group 1', 'AY19/20S1', 'CS2103');
+INSERT INTO groups VALUES ('Group 2', 'AY19/20S1', 'CS2103');
+INSERT INTO groups VALUES ('Group 3', 'AY19/20S1', 'CS2103');
+
+INSERT INTO groups VALUES ('Group 1', 'AY18/19S1', 'GEQ1000');
+INSERT INTO groups VALUES ('Group 2', 'AY18/19S1', 'GEQ1000');
+INSERT INTO groups VALUES ('Group 3', 'AY18/19S1', 'GEQ1000');
+INSERT INTO groups VALUES ('Group 1', 'AY18/19S2', 'GEQ1000');
+INSERT INTO groups VALUES ('Group 2', 'AY18/19S2', 'GEQ1000');
+INSERT INTO groups VALUES ('Group 3', 'AY18/19S2', 'GEQ1000');
+INSERT INTO groups VALUES ('Group 1', 'AY19/20S1', 'GEQ1000');
+INSERT INTO groups VALUES ('Group 2', 'AY19/20S1', 'GEQ1000');
+INSERT INTO groups VALUES ('Group 3', 'AY19/20S1', 'GEQ1000');
+INSERT INTO groups VALUES ('Group 1', 'AY19/20S2', 'GEQ1000');
+INSERT INTO groups VALUES ('Group 2', 'AY19/20S2', 'GEQ1000');
+INSERT INTO groups VALUES ('Group 3', 'AY19/20S2', 'GEQ1000');
 
 /* GROUP_MEMBERSHIPS */
 
 /* FORUMS */
+INSERT INTO forums VALUES ('Lectures', 'AY18/19S1', 'CS2102');
+INSERT INTO forums VALUES ('Tutorials', 'AY18/19S1', 'CS2102');
+INSERT INTO forums VALUES ('Assignments', 'AY18/19S1', 'CS2102');
+INSERT INTO forums VALUES ('Lectures', 'AY18/19S2', 'CS2102');
+INSERT INTO forums VALUES ('Tutorials', 'AY18/19S2', 'CS2102');
+INSERT INTO forums VALUES ('Assignments', 'AY18/19S2', 'CS2102');
+
+INSERT INTO forums VALUES ('Lectures', 'AY18/19S1', 'CS1101S');
+INSERT INTO forums VALUES ('Tutorials', 'AY18/19S1', 'CS1101S');
+INSERT INTO forums VALUES ('Assignments', 'AY18/19S1', 'CS1101S');
+INSERT INTO forums VALUES ('Lectures', 'AY18/19S2', 'CS1101S');
+INSERT INTO forums VALUES ('Tutorials', 'AY18/19S2', 'CS1101S');
+INSERT INTO forums VALUES ('Assignments', 'AY18/19S2', 'CS1101S');
+
+INSERT INTO forums VALUES ('Lectures', 'AY18/19S1', 'CS2103');
+INSERT INTO forums VALUES ('Tutorials', 'AY18/19S1', 'CS2103');
+INSERT INTO forums VALUES ('Assignments', 'AY18/19S1', 'CS2103');
+INSERT INTO forums VALUES ('Lectures', 'AY19/20S1', 'CS2103');
+INSERT INTO forums VALUES ('Tutorials', 'AY19/20S1', 'CS2103');
+INSERT INTO forums VALUES ('Assignments', 'AY19/20S1', 'CS2103');
+
+INSERT INTO forums VALUES ('Lectures', 'AY18/19S1', 'GEQ1000');
+INSERT INTO forums VALUES ('Tutorials', 'AY18/19S1', 'GEQ1000');
+INSERT INTO forums VALUES ('Assignments', 'AY18/19S1', 'GEQ1000');
+INSERT INTO forums VALUES ('Lectures', 'AY18/19S2', 'GEQ1000');
+INSERT INTO forums VALUES ('Tutorials', 'AY18/19S2', 'GEQ1000');
+INSERT INTO forums VALUES ('Assignments', 'AY18/19S2', 'GEQ1000');
+INSERT INTO forums VALUES ('Lectures', 'AY19/20S1', 'GEQ1000');
+INSERT INTO forums VALUES ('Tutorials', 'AY19/20S1', 'GEQ1000');
+INSERT INTO forums VALUES ('Assignments', 'AY19/20S1', 'GEQ1000');
+INSERT INTO forums VALUES ('Lectures', 'AY19/20S2', 'GEQ1000');
+INSERT INTO forums VALUES ('Tutorials', 'AY19/20S2', 'GEQ1000');
+INSERT INTO forums VALUES ('Assignments', 'AY19/20S2', 'GEQ1000');
 
 /* ACCESSES */
 
