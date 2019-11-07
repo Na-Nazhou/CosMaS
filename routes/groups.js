b02878af3307a80535b1a9796d36d0ef8b6549c0
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2019-11-08 04:07:11 +0800

    Minor changes in groups routes

--- routes/groups.js
+++ routes/groups.js
@@ -5,7 +5,7 @@ const { ensureAuthorised } = require('../permissions');
 const { canShowGroup, canCreateGroup, canUpdateGroup, canDeleteGroup } = require('../permissions/groups');
 
 router.use((req, res, next) => {
-  log.controller('groups controller handling the request');
+  log.controller('Groups controller handling the request');
   next();
 });
 
@@ -14,15 +14,15 @@ router.get(
   ensureAuthorised(req => canCreateGroup(req.user, req.params.semester_name, req.params.module_code)),
   groups.new
 );
-router.get(
-  '/:name',
-  ensureAuthorised(req => canShowGroup(req.user, req.params.semester_name, req.params.module_code, req.params.name)),
-  groups.show
-);
 router.post(
   '/',
   ensureAuthorised(req => canCreateGroup(req.user, req.params.semester_name, req.params.module_code)),
   groups.create
+);
+router.get(
+  '/:name',
+  ensureAuthorised(req => canShowGroup(req.user, req.params.semester_name, req.params.module_code, req.params.name)),
+  groups.show
 );
 router.delete(
   '/:name',
