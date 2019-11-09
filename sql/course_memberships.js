217fd0ec3661131d14df8d0405f9e40148e1aa82
Na-Nazhou <nazhou.na9@gmail.com>
2019-11-09 11:11:17 +0800

    Fix get current semester

--- sql/course_memberships.js
+++ sql/course_memberships.js
@@ -10,9 +10,13 @@ course_memberships.queries = {
     'SELECT * FROM users WHERE NOT is_admin AND id NOT IN ' +
     '(SELECT user_id FROM course_memberships WHERE semester_name=$1 AND module_code=$2)',
   get_courses_by_user:
-    'SELECT DISTINCT C.semester_name, C.module_code, C.title, C.description, C.credits, CM.role FROM courses C, ' +
-    'course_memberships CM WHERE C.semester_name = CM.semester_name AND C.module_code = CM.module_code AND ' +
-    'CM.user_id = $1 ORDER BY semester_name, module_code'
+    'SELECT DISTINCT S.start_time, S.end_time, C.semester_name, C.module_code, C.title, C.description, C.credits, CM.role ' +
+    'FROM courses C, course_memberships CM, semesters S ' +
+    'WHERE S.name = C.semester_name AND C.semester_name = CM.semester_name AND C.module_code = CM.module_code AND ' +
+    'CM.user_id = $1 ORDER BY semester_name, module_code',
+  get_members_by_course:
+    'SELECT * FROM course_memberships C JOIN users U ON C.user_id = U.id ' +
+    'WHERE semester_name = $1 AND module_code=$2 AND role=$3'
 };
 
 course_memberships.functions = {
